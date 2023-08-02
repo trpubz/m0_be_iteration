cbffe541077419eefe02f95bca227f042490fa29
Taylor Pubins <tpubz@icloud.com>
2023-08-02 08:55:29 -0700

    Refactor each method to print words that are exactly 3 characters long

--- each.rb
+++ each.rb
@@ -10,6 +10,7 @@ weather_options.each { |w| puts "It's #{w} today." }
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
-words.each { |w| w.length == 3 ? puts("#{w} is exactly 3 characters in length") : nil }
+words.each { |w| w.length == 3? puts(w) : nil }
+
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
